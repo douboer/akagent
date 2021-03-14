6e591acb7d2c68928e7e5dd325849557f001e8ad
mangosteen <1774847429@qq.com>
2021-03-15 05:09:26 +0800

    调整timestamp类型

--- example/process.c
+++ example/process.c
@@ -10,7 +10,7 @@ static int process_callback(unsigned char *buffer ,unsigned int size)
     akfs_process_t *p = NULL;
 
     p = (akfs_process_t *)buffer;
-    printf("type:[%d] pid %d ppid %d tgid %d tpath:[%s] ns:[%u] args:[%s] hash:[%s] timestamp[%s]\n" ,p->data_type ,p->pid ,p->ppid ,p->gid ,
+    printf("type:[%d] pid %d ppid %d tgid %d tpath:[%s] ns:[%u] args:[%s] hash:[%s] timestamp[%ld]\n" ,p->data_type ,p->pid ,p->ppid ,p->gid ,
         p->exec_file ,p->ns ,p->argv ,p->exec_hash ,p->timestamp);
 
     return 0;
