2c67cc353ee7ca04dbffffb69d00fb245d217f4d
phil-fly <712127467@qq.com>
2021-06-11 13:40:32 +0800

    网络监控添加字段,修复序列化问题

--- src/monitor/net.go
+++ src/monitor/net.go
@@ -105,40 +105,49 @@ func (n *NetMonitor) Report() {
 
 //NetEvent 网络监控接口字段
 type NetEvent struct {
-	Timestamp uint64 `json:"timestamp"`                       //事件时间戳
-	Data_type uint32 `json:"data_type" enum:"1001,1002,1003"` //数据类型
-	Pid       uint32 `json:"pid"`                             //进程id
-	Uid       uint32 `json:"uid"`                             //用户ID
-	UserName  string `json:"user_name"`                       //用户名
-	Gid       uint32 `json:"gid"`                             //用户组ID
-	GroupName string `json:"group_name"`                      //组名
-	Namespace uint32 `json:"namespace"`                       //进程命名空间ID
-	SrcIp	string	`json:"src_ip"`							//源地址
-	DstIp	string	`json:"dst_ip"`							//目的地址
-	SrcPort	uint16	`json:"src_port"`						//源端口
-	DstPort	uint16	`json:"dst_port"`						//目的端口
-	Exe_file  string `json:"exe_file"`                        //进程文件
+	Timestamp   uint64 `json:"timestamp"`                       //事件时间戳
+	Data_type   uint32 `json:"data_type" enum:"3001,3002,3003"` //数据类型
+	Pid         uint32 `json:"pid"`                             //进程id
+	Exe_file    string `json:"exe_file"`                        //进程文件
+	Parent_pid  uint32 `json:"parent_pid"`                      //父进程id
+	Parent_name string `json:"parent_name"`                     //父进程文件
+	Ptgid       uint32 `json:"ptgid"`                           //进程tgid
+	Uid         uint32 `json:"uid"`                             //用户ID
+	UserName    string `json:"user_name"`                       //用户名
+	Gid         uint32 `json:"gid"`                             //用户组ID
+	GroupName   string `json:"group_name"`                      //组名
+	Namespace   uint32 `json:"namespace"`                       //进程命名空间ID
+	SrcIp       string `json:"src_ip"`                          //源地址
+	DstIp       string `json:"dst_ip"`                          //目的地址
+	SrcPort     uint16 `json:"src_port"`                        //源端口
+	DstPort     uint16 `json:"dst_port"`                        //目的端口
 }
+
 
 //NetEvent 网络数据解析
 func (n *NetEvent) New(monitorData []byte) {
 	offset := 0
 	n.Timestamp = binary.LittleEndian.Uint64(monitorData[offset : offset+8])
 	offset += 8
-
+	//1
 	n.Data_type = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
 
 	n.Pid = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
 
+	n.Parent_pid = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
+	offset += 4
+
+	n.Ptgid = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
+	offset += 4
+	//2
 	n.Uid = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
 	usr, err := user.LookupId(fmt.Sprintf("%d", n.Uid))
 	if err == nil {
 		n.UserName = usr.Username
 	}
-
 
 	n.Gid = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
@@ -147,33 +156,34 @@ func (n *NetEvent) New(monitorData []byte) {
 		n.GroupName = Ginfo.Name
 	}
 
-
+	//3
 	n.Namespace = binary.LittleEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
 
 	//网络四元组
 	SrcIp := binary.BigEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
-	n.SrcIp = InetNtoA(SrcIp)
-
+	n.SrcIp = IP2String(SrcIp)
+	//4
 	DstIp := binary.BigEndian.Uint32(monitorData[offset : offset+4])
 	offset += 4
-	n.DstIp = InetNtoA(DstIp)
+	n.DstIp = IP2String(DstIp)
 
 	n.SrcPort = binary.BigEndian.Uint16(monitorData[offset : offset+2])
 	offset += 2
 	n.DstPort = binary.BigEndian.Uint16(monitorData[offset : offset+2])
 	offset += 2
 
-
+	//5
+	n.Parent_name = string(bytes.Trim(monitorData[offset:offset+64], "\x00"))
+	offset += 64
 
 	n.Exe_file = string(bytes.Trim(monitorData[offset:offset+256], "\x00"))
 	offset += 256
 
-
 }
 
-func InetNtoA(ip uint32) string {
+func IP2String(ip uint32) string {
 	return fmt.Sprintf("%d.%d.%d.%d",
 		byte(ip>>24), byte(ip>>16), byte(ip>>8), byte(ip))
 }
